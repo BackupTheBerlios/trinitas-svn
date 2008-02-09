171dd77b8a96afe68be7b4065371d12aa1512f5f
ichthys1 <ichthys1@ab3bda7c-5b37-0410-9911-e7f4556ba333>
2008-02-09 23:23:14 +0000

    Anlegen fehlender Dateien und aktualisieren des Copyright

---
+++ trunk/source/base/Map.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2008 by Trinitas. All rights reserved.
+ * Distributed under the terms of the BSD License.
+ *
+ * Authors:
+ *		Developer1 Name1, optiona1@email
+ *		Developer2 Name2, optiona2@email
+ */
